5b2c0b0d09b27346de4015ae20267c374fad5b86
marblekang <kangbm930@gmail.com>
2022-03-16 15:50:57 +0900

    03/16 redux 사용해서 재고 증감 추가

--- src/Cart.js
+++ src/Cart.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
+import {connect} from 'react-redux';
 
-function Cart(){
+function Cart(props){
   return (
     <div>
       <Table responsive>
         <tr>
-          <th>#</th>
+          <th>id</th>
           <th>상품명</th>
           <th>수량</th>
           <th>변경</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-        </tr>
+        { props.prodInfo.map((a,i)=>{
+          return(
+          <tr key={i}>
+            <td>{a.id}</td>
+            <td>{a.name}</td>
+            <td>{a.quan}</td>
+            <td><button className="btn" onClick={()=>{props.dispatch({type:'incQuan'})}}>+</button>
+                <button className="btn" onClick={()=>{props.dispatch({type:'decQuan'})}}>-</button>
+            </td>
+          </tr> )
+        })}
       </Table>
+        { props.alert === true
+          ? (<div className="my-alert">
+              <p>지금 구매하시면 20% 할인</p>
+              <button onClick={()=>{props.dispatch({type:'closeAlert'})}}>닫기</button>
+            </div>)
+          :null  
+          }     
     </div>
   )
 }
 
-export default Cart;
+// store 안에 있는 state 를 props로 만들어 주는 함수
+function stateToProps(state){
+  return {
+    prodInfo: state.reducer,
+    alert : state.reducer2
+  }
+}
+
+
+export default connect(stateToProps)(Cart);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,53 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {combineReducers, createStore} from 'redux';
 
+// 상품정보
+let prodInfo = [{id:0, name:'awesomeShoes', quan:2},{id:1, name:'awesomeSnack', quan:1}]
+
+// reducer - 수량 증감을 위한 state
+function reducer(state = prodInfo, action){
+  let copy = [...state];
+
+  if (action.type === 'incQuan'){
+
+   copy[0].quan++;
+    return copy;
+  }else if(action.type === 'decQuan'){
+  
+    copy[0].quan--;
+    return copy;
+  }else{
+    return state;
+  }
+}
+
+// reducer - 알림창 상태 저장해서 보여주고 숨기는 state
+let alertUi = true;
+
+function reducer2(state = alertUi,action){
+  if (action.type === 'closeAlert'){
+    return false;
+  }else{
+    return state;
+  }
+}
+///////////
+
+
+// redux 로 state 만들기 - createStore  
+                              //combineReducers - reducer 여러개 만들어야 될때 사용.
+let store = createStore( combineReducers({reducer,reducer2}));
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store = {store}>
     <App />
+    </Provider> 
+    {/* Provider import 한 후에 state값 공유를 원하는 컴포넌트를 다 감싸면 됨. */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
