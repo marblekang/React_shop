db147032882e3f0450dfe5a7e933d5d8b0b2c6ad
marblekang <kangbm930@gmail.com>
2022-03-18 15:38:59 +0900

    03/18 useSector , useDispatch  추가

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
-import {connect} from 'react-redux';
+import {connect, useDispatch, useSelector} from 'react-redux';
+
 
 function Cart(props){
+
+  let state = useSelector((state)=>{
+    return state})
+    console.log(state.reducer) // reducer == prodInfo , reducer2 == alertUi (true)
+  let dispatch = useDispatch()
+
   return (
     <div>
       <Table responsive>
@@ -12,14 +19,14 @@ function Cart(props){
           <th>수량</th>
           <th>변경</th>
         </tr>
-        { props.prodInfo.map((a,i)=>{
+        { state.reducer.map((a,i)=>{
           return(
           <tr key={i}>
             <td>{a.id}</td>
             <td>{a.name}</td>
             <td>{a.quan}</td>
-            <td><button className="btn" onClick={()=>{props.dispatch({type:'incQuan'})}}>+</button>
-                <button className="btn" onClick={()=>{props.dispatch({type:'decQuan'})}}>-</button>
+            <td><button className="btn" onClick={()=>{dispatch({type:'incQuan'})}}>+</button>
+                <button className="btn" onClick={()=>{dispatch({type:'decQuan'})}}>-</button>
             </td>
           </tr> )
         })}
@@ -27,7 +34,7 @@ function Cart(props){
         { props.alert === true
           ? (<div className="my-alert">
               <p>지금 구매하시면 20% 할인</p>
-              <button onClick={()=>{props.dispatch({type:'closeAlert'})}}>닫기</button>
+              <button onClick={()=>{dispatch({type:'closeAlert'})}}>닫기</button>
             </div>)
           :null  
           }     
@@ -36,7 +43,7 @@ function Cart(props){
 }
 
 // store 안에 있는 state 를 props로 만들어 주는 함수
-function stateToProps(state){
+/* function stateToProps(state){
   return {
     prodInfo: state.reducer,
     alert : state.reducer2
@@ -44,4 +51,7 @@ function stateToProps(state){
 }
 
 
-export default connect(stateToProps)(Cart);
+export default connect(stateToProps)(Cart); */
+
+// useSelector 사용
+export default Cart;
