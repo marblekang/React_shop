17f36051c126a171b3acd6c41b20236a3fa681e4
marblekang <kangbm930@gmail.com>
2022-03-20 22:28:57 +0900

    03/20 reducer 수정

--- src/Cart.js
+++ src/Cart.js
@@ -31,7 +31,7 @@ function Cart(props){
           </tr> )
         })}
       </Table>
-        { props.alert === true
+        { state.reducer2 === true
           ? (<div className="my-alert">
               <p>지금 구매하시면 20% 할인</p>
               <button onClick={()=>{dispatch({type:'closeAlert'})}}>닫기</button>
